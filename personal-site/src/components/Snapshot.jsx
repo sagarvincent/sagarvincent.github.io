2ac181e2662c9189a43dc889910f23e980f29d6e
Sagar Vincent <sagarvincent@gmail.com>
2024-06-24 14:31:51 +0100

    Made a snapshot section in site

---
+++ personal-site/src/components/Snapshot.jsx
@@ -0,0 +1,35 @@
+import './Snapshot.css';
+
+
+function Snapshot(){
+    return (
+        <>
+            <snapshot className="sscontainer">
+                <h3>Education</h3>
+                <ul><li>MSc. Robotics <br/>University of Birmingham<br/>2022-23</li></ul>
+                <h3>Experience</h3>
+                <ul><li>AI engineer <br/>Bubblebit<br/>2024-present</li>
+                <li>Data Analyst<br/>University of Birmingham<br/>2023-present</li>
+                <li>Programmer <br/>Cognizant Technical Solutions<br/>2020-22</li></ul>
+                <h3>Skills</h3>
+                <ul>
+                    <li>Programming</li>
+                    <li>Frameworks</li>
+                    <li>Tools</li>
+                    <li>Soft</li>
+                </ul>
+                <h3>Hobbies</h3>
+                <ul>
+                    <li>MotorCycling</li>
+                    <li>Reading</li>
+                    <li>Movies</li>
+                    <li>Deep talks</li>
+                    <li>Food Hunting</li>
+                </ul>          
+            </snapshot>
+        </>
+        
+    );
+}
+
+export default Snapshot;
