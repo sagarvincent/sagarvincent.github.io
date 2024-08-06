5d0fe18a83730e6f644d613f5a6ecf30c2242236
sagarvincent <sagarvincent@gmail.com>
2024-08-06 19:36:35 +0100

    Completed basic outline

--- personal-site/src/components/CustomTextBox.js
+++ personal-site/src/components/CustomTextBox.js
@@ -5,12 +5,12 @@ function CustomTextBox() {
       <>
       <div className="CustomTextBox">
         <p>I'm Tech Architect merging 
-        <div className="DDE ">DEVELOPMENT, DESIGN & ENGINEERING</div>
+        <div className="DDE1 ">DEVELOPMENT, DESIGN & ENGINEERING</div>
         to bring ideas to life.</p>
       </div>
       <p className="b2">
       CHECK OUT MY WORKS
-      <div className="DDE">AI & ROBOTICS <span>&#8599;</span>  DESIGNING  <span>&#8599;</span>  DEVELOPMENT
+      <div className="DDE2">AI & ROBOTICS <span>&#8599;</span>  DESIGNING  <span>&#8599;</span>  DEVELOPMENT
       </div>
     </p>
     </>
